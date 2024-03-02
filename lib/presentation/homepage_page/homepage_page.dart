ded7b447707ad897a7994009ff85aaea00d6c5e7
Muhammad Anees <anees7757@gmail.com>
2024-03-02 17:47:02 +0500

    login/signup

--- lib/presentation/homepage_page/homepage_page.dart
+++ lib/presentation/homepage_page/homepage_page.dart
@@ -260,7 +260,7 @@ class HomepagePageState extends State<HomepagePage> {
             ),
             actions: [
               AppbarTrailingIconbutton(
-                imagePath: ImageConstant.imgNavTransaction,
+                imagePath: ImageConstant.imgVector,
                 margin: EdgeInsets.only(
                   left: 19.h,
                   top: 2.v,
